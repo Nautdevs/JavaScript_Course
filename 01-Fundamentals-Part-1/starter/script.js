4d4334b25d19b195869845e38ae5fd54b18c9469
Nautdevs <78232322+Nautdevs@users.noreply.github.com>
2024-06-29 20:45:06 -0600

    "Yolo"

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,61 @@
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Calvin');
+
+javascriptIsFun = 'New Value, just type changes the variable javascriptIsFun';
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+// The value is undefined and the type of the variable is undefined
+console.log(typeof year);
+
+let year_two = '';
+console.log(typeof year);
+
+console.log(typeof null);
+ */
+
+/* let age = 30;
+// This is called mutating a variable
+age = 31;
+
+const birthYear = 1998;
+ */
+// const birthYear = 1999; This will cause an assignment error
+// const birthYear; This will cause a declaration error
+/*
+const currentYear = 2024;
+const ageCalvin = currentYear - 1998;
+console.log(ageCalvin);
+
+const ageHeather = currentYear - 1999;
+console.log(ageHeather);
+console.log(ageCalvin, ageHeather);
+
+// We have x++ and x+= number and x-- x*=
+
+let x = 10 + 5;
+console.log(x);
+x += 10;
+console.log(x);
+x *= 4;
+console.log(x);
+x++;
+console.log(x);
+x--;
+console.log(x);
+x--;
+console.log(x);
+
+*/
+firstName = 'Calvin';
+const calvin = "I'm " + firstName + '.';
+console.log(calvin);
+
+const calvinNew = `I'm ${firstName}. Check this out`;
+console.log(calvinNew);
---
+++ 01-Fundamentals-Part-1/starter/script_old.js
@@ -0,0 +1,15 @@
+let js = 'amazing'
+console.log(40+8+23-10)
+
+// Step 1: Declare a variable
+let first_name = "Jonas"
+
+console.log(first_name)
+console.log(first_name)
+console.log(first_name)
+console.log(first_name)
+
+// Can't use keywords, new & name are both keywords
+// let new = "yes", 
+// No first letter being capital (This is a convention)
+
